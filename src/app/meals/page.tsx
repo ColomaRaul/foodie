966aed03c6412e570ff0d4ec32b10f8e53e7e05b
Raúl Coloma <rau17cb@gmail.com>
2024-03-03 18:47:55 +0100

    add page meals

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import classes from './page.module.css'
 import Link from 'next/link'
 import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/libs/meals'
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +20,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   )
