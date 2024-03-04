527c1946eb4f7bb72645537f14935da4d258335d
Raúl Coloma <rau17cb@gmail.com>
2024-03-04 17:31:57 +0100

    not found

--- src/libs/meals.tsx
+++ src/libs/meals.tsx
@@ -1,3 +1,4 @@
+import { MealDB } from '@/interfaces/db_interfaces';
 import sql from 'better-sqlite3';
 
 const db = new sql('meals.db');
@@ -6,4 +7,8 @@ export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   //throw new Error('Not able to fetch meals');
   return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMealBySlug(slug: string) {
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
